1cad1b6bffa45ded763fd860a603c871f082b7f6
FalconDesign <hebgax0@gmail.com>
2016-09-21 01:38:04 +0300

    added spinning menu

--- website/app/scripts/functions.js
+++ website/app/scripts/functions.js
@@ -5,7 +5,7 @@ $(() => {
 movingBg();
 currencyDropDown();
 smoothScroll();
-
+entranceEvt();
 });
 
 // DropDown menu in the section content and reserve
@@ -25,7 +25,7 @@ let currencyDropDown = () => {
 // Function for smoothscroling anchor
 
 let smoothScroll = () => {
-  $(document).on('click', 'a', (evt) => {
+  $(document).on('click', 'a[href^="#contact"]', (evt) => {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $( $.attr(evt.currentTarget, 'href') ).offset().top
@@ -48,4 +48,28 @@ let movingBg = () => {
   let $window = $(window);
   $window.on('scroll', parallaxBg);
 
+};
+
+// Entrance event
+
+let entranceEvt = () => {
+  let anchorEntrance = $('a[href^="#entrance"]'),
+      currencyStripe = $('.currency-stripe'),
+      entranceForm   = $('#entrance'),
+      closeImg       = $('.close'),
+      header         = $('.main-header');
+
+
+  closeImg.on('click', () => {
+    header.removeClass('dark');
+    currencyStripe.removeClass('move-down');
+    entranceForm.removeClass('show');
+  });
+
+
+  anchorEntrance.on('click', () => {
+    header.toggleClass('dark');
+    currencyStripe.toggleClass('move-down');
+    entranceForm.toggleClass('show');
+  });
 };
