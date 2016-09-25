45013cae853ceb87892cc42ae115fcb885b6b4b1
FalconDesign <hebgax0@gmail.com>
2016-09-25 22:37:04 +0300

    Fixed animation of the splash pages

--- website/app/scripts/functions.js
+++ website/app/scripts/functions.js
@@ -6,9 +6,10 @@ movingBg();
 currencyDropDown();
 smoothScroll();
 entranceEvt();
+
 });
 
-// DropDown menu in the section content and reserve
+// DropDown menu in the sections content and reserve
 
 let currencyDropDown = () => {
   let switcher        = $('.switcher');
@@ -22,10 +23,10 @@ let currencyDropDown = () => {
     });
 };
 
-// Function for smoothscroling anchor
+// Function for smoothScrolling anchor
 
 let smoothScroll = () => {
-  $(document).on('click', 'a[href^="#contact"]', (evt) => {
+  $(document).on('click', 'a[href^="#contact"], a[href^="#about"], a[href^="#content"]', (evt) => {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $( $.attr(evt.currentTarget, 'href') ).offset().top
@@ -33,16 +34,16 @@ let smoothScroll = () => {
   });
 };
 
-// Moving bg. Parallax
+// Moving bg. Parralax
 
 let movingBg = () => {
 
   let parallaxBg = () => {
-    let contact       = $('#contact'),
+    let content       = $('#content'),
         about         = $('#about'),
         windowScroll  = $(window).scrollTop();
-    contact.css({ backgroundPosition: 'center ' + (160 + (-windowScroll / 2)) + 'px' });
-    about.css({ backgroundPosition: 'center ' + (660 + (-windowScroll / 2)) + 'px' });
+    content .css({backgroundPosition: 'center ' + (-windowScroll / 2) + 'px' });
+    about   .css({backgroundPosition: 'center ' + (560 + (-windowScroll / 2 )) + 'px'});
   };
 
   let $window = $(window);
@@ -53,23 +54,79 @@ let movingBg = () => {
 // Entrance event
 
 let entranceEvt = () => {
-  let anchorEntrance = $('a[href^="#entrance"]'),
-      currencyStripe = $('.currency-stripe'),
-      entranceForm   = $('#entrance'),
-      closeImg       = $('.close'),
-      header         = $('.main-header');
+  let anchorEntrance  = $('a[href^="#entrance"]'),
+      anchorFAQ       = $('a[href^="#faq"]'),
+      anchroTarify    = $('a[href^="#tarify"]'),
+      entranceForm    = $('#entrance'),
+      popUpContainer  = $('.pop-up-container'),
+      confidentiality = $('#confidentiality'),
+      faq             = $('#faq'),
+      tarify          = $('#tarify'),
+      closeImg        = $('.close'),
+      header          = $('.main-header-fixed'),
+      popUp           = $('.popup'),
+      shadow          = $('.shadow');
 
+  popUp.on('click', (evt) => {
+    let $this = $(evt.currentTarget);
+    let setOne = [header, shadow, popUpContainer];
+    for (let item of setOne) {
+      item.fadeIn(500);
+    }
 
-  closeImg.on('click', () => {
-    header.removeClass('dark');
-    currencyStripe.removeClass('move-down');
-    entranceForm.removeClass('show');
+    if ($this.attr('href') == '#entrance') {
+      if (!entranceForm.is(':visible')) {
+        entranceForm .siblings().fadeOut(500);
+        entranceForm .fadeIn(500);}
+
+      else if(entranceForm.is(':visible')) {
+        let setTwo = [entranceForm, popUpContainer, header]
+        for (let item of setTwo) {
+          item.fadeOut(500)
+        }
+      }
+
+    } else if ($this.attr('href') == '#faq') {
+        if (!faq.is(':visible')) {
+          faq.siblings().fadeOut(500);
+          faq.fadeIn(500);}
+
+        else if(faq.is(':visible')) {
+          let setThree = [faq, popUpContainer, header];
+          for (let item of setThree) {
+            item.fadeOut(500);
+          }
+        }
+    } else if ($this.attr('href') == '#tarify') {
+        if (!tarify.is(':visible')) {
+          tarify.siblings().fadeOut(500);
+          tarify.fadeIn(500);}
+
+        else if(tarify.is(':visible')) {
+          let setFour = [tarify, popUpContainer, header];
+          for (let item of setFour) {
+            item.fadeOut(500);
+          }
+        }
+
+    } else if ($this.attr('href') == '#confidentiality') {
+        if (!confidentiality.is(':visible')) {
+          confidentiality.siblings().fadeOut(500);
+          confidentiality.fadeIn(500);}
+
+        else if(confidentiality.is(':visible')) {
+          let setFive = [confidentiality, popUpContainer, header];
+          for (let item of setFive) {
+            item.fadeOut(500);
+        }
+      }
+    }
   });
 
-
-  anchorEntrance.on('click', () => {
-    header.toggleClass('dark');
-    currencyStripe.toggleClass('move-down');
-    entranceForm.toggleClass('show');
+    closeImg.on('click', () => {
+      let toClose = [entranceForm, faq, tarify, confidentiality, popUpContainer, header];
+      for (let item of toClose) {
+        item.fadeOut(500);
+      }
   });
 };
